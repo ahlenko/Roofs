a0547f89d1591ec46fabb4f4947700f9d49ef03c
ahlenko <ahlenko.02@gmail.com>
2024-11-04 11:49:45 +0200

    Add orders screen design

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1411242982"
+    serviceWorkerVersion: "3449373293"
   }
 });
