f46291f7f2a034653e6228c554a90e0cf1fd3d97
ahlenko <ahlenko.02@gmail.com>
2024-11-08 16:51:56 +0200

    Add landing main page (need add calculator)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3449373293"
+    serviceWorkerVersion: "884826126"
   }
 });
